869d3f5ecb44c8f53df8918bbf3c89fab0bd2735
TaiwoOmisakin <adebolaruth736@gmail.com>
2025-07-03 15:22:12 +0100

    Dice_Rolling Game.py

--- Dice_Rolling Game.py
+++ Dice_Rolling Game.py
@@ -1,7 +1,7 @@
 import random
 
 while True:
-    choice= input("Roll the dice? (y/n): ").upper
+    choice= input("Roll the dice? (y/n): ").upper()
     if choice == "Y":
         no1= random.randint(1,6)
         no2= random.randint(1,6)
